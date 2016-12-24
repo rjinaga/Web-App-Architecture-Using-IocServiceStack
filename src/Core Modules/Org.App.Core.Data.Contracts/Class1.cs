ed542ce058b2672ad51692e5dea6d665edc97ca5
rjinaga <rjinaga@gmail.com>
2016-12-24 10:27:30 +0530

    initial commit

---
+++ src/Core Modules/Org.App.Core.Data.Contracts/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Org.App.Core.Data.Contracts
+{
+    public class Class1
+    {
+    }
+}
