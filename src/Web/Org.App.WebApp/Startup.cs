3dc9927c4689eea43ecb7ed434c616e5e5147c64
rjinaga <rjinaga@gmail.com>
2016-12-24 12:04:10 +0530

    Added references

--- src/Web/Org.App.WebApp/Startup.cs
+++ src/Web/Org.App.WebApp/Startup.cs
@@ -68,5 +68,14 @@ namespace NjetInterserviceWebApplication
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+        private void RegisterServices()
+        {
+            var configRef = NJet.Interservice.ServiceInjector.Configure(config =>
+            {
+                config.AddServices((serviceConfig) =>{serviceConfig.Assemblies = new[] { "Org.App.BusinessService" }; })
+                      .AddDependentServices((serviceConfig) => { serviceConfig.Assemblies = new[] { "Org.App.DataService" }; });
+            });
+        }
     }
 }
